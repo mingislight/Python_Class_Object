4663b207c852a7ad4eb810c9d59691e92f78e10d
mingislight <MINGCOMPUTERSCIENCE@GMAIL.COM>
2025-02-18 10:50:17 -0700

    Added Slots, Multi-inheritance and MRO

--- multi_inheritance_MRO.py
+++ multi_inheritance_MRO.py
@@ -1,4 +1,5 @@
-# Multiple Inheritance through difference classes
+# Multiple Inheritance 
+# Method Resolution Order (MRO), first parameter first, second second
 
 class Employee:
     def __init__(self, name, age, salary):
@@ -6,14 +7,14 @@ class Employee:
         self.age = age
         self.salary = salary
 
-    def increse_salary(self, percent):
-        self.salary += self.salary * (percent/100)
+    def increase_salary(self, percent):
+        self.salary += self.salary * (percent / 100)
 
 class SlotsInspectorMixin:
     def has_slots(self):
         return hasattr(self, "__slots__")
 
-class Developer(SlotsInspectorMixin, Employee):
+class Developer(SlotsInspectorMixin, Employee):  # MRO: SlotsInspectorMixin → Employee
     __slots__ = ("framework", )
 
     def __init__(self, name, age, salary, framework):
@@ -21,13 +22,9 @@ class Developer(SlotsInspectorMixin, Employee):
         self.framework = framework
 
     def increase_salary(self, percent, bonus=0):
-        super().increase_salary(percent)
+        super().increase_salary(percent)  # Calls Employee's increase_salary()
         self.salary += bonus
 
-    def has_slots(self):
-        return hasattr(self, "__slots__")
-
-
 employee1 = Developer("Ji-Soo", 38, 1000, "Flask")
-print(employee1.has_slots())
-print(employee1.__dict__) # Method's location
+print(employee1.has_slots())  # Expected: True
+print(Developer.__mro__)  # Print Method Resolution Order
